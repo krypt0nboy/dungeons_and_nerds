5cb7233e5ce5b9e50a3b1a6c6966ca6a0b25acdb
Harold Cohen <h.cohen.apple@icloud.com>
2021-02-28 21:47:51 +0100

    Addding utils for characters module

---
+++ dnn/game_engine/characters/utils.py
@@ -0,0 +1,12 @@
+"""This file provides with character related utility functions.
+
+"""
+
+
+def lock_character(character=None):
+    """
+    :param character: The character to lock.
+    :return: A Lock if the character was successfully locked.
+    :rtype: dnn.game_engine.characters.locks.CharacterLockBase
+    """
+    pass
