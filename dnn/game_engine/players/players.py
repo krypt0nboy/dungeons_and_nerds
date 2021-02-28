3f5ccbb31db34268f0cc61bf7e6f76aced14ff8f
Harold Cohen <h.cohen.apple@icloud.com>
2021-02-28 01:29:35 +0100

    Adding properties

--- dnn/game_engine/players/players.py
+++ dnn/game_engine/players/players.py
@@ -10,18 +10,31 @@ class PlayerBase(object):
     Base class for Player.
     """
 
-    def __init__(self, nickname=None, active_character=None, active_character_controller=None):
-        self._nickname = nickname
+    def __init__(self, username=None, active_character=None, active_character_controller=None):
+        """
+        :param str username: The player's username.
+        :param active_character: The player's active (selected) character.
+        :param active_character_controller: The player's active character controller.
+        """
+        self._username = username
         self._active_character = active_character
         self._active_character_controller = active_character_controller
 
     @property
-    def nickname(self):
-        return self._nickname
+    def username(self):
+        return self._username
 
-    @nickname.setter
-    def nickname(self, value):
-        self._nickname = value
+    @username.setter
+    def username(self, value):
+        self._username = value
+
+    @property
+    def active_character(self):
+        return self._active_character
+
+    @property
+    def active_character_controller(self):
+        return self._active_character_controller
 
     def select_character(self, character=None):
         pass
