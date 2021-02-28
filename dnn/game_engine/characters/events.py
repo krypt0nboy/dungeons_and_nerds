914f2a05f71175fc19fa112ae05c37096f37bcf8
Harold Cohen <h.cohen.apple@icloud.com>
2021-02-28 01:28:12 +0100

    Adding CharacterHasBeenLockedEvent

--- dnn/game_engine/characters/events.py
+++ dnn/game_engine/characters/events.py
@@ -8,7 +8,13 @@ class CharacterEvent(EventBase):
     """
     The base class for Character events.
     """
-    pass
+
+    def __init__(self, character=None):
+        self._character = character
+
+    @property
+    def character(self):
+        return self._character
 
 
 class CharacterHasBeenPromotedEvent(CharacterEvent):
@@ -37,3 +43,17 @@ class CharacterHasUpgradedSkills(CharacterEvent):
     An event that occurs after a character has upgraded his skills.
     """
     pass
+
+
+class CharacterHasBeenLockedEvent(CharacterEvent):
+    """
+    An event that occurs after a character has been locked.
+    """
+
+    def __init__(self, lock=None, **kwargs):
+        super(CharacterHasBeenLockedEvent, self).__init__(**kwargs)
+        self._lock = lock
+
+    @property
+    def lock(self):
+        return self._lock
