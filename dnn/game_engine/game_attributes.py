80d3f996e38c48447a9b570f7e32cc178b9063c8
Harold Cohen <h.cohen.apple@icloud.com>
2021-03-01 22:04:02 +0100

    Adding default to game attributes

--- dnn/game_engine/game_attributes.py
+++ dnn/game_engine/game_attributes.py
@@ -9,12 +9,14 @@ class GameAttributeBase(object):
     Base class for GameAttribute.
     """
 
-    def __init__(self, name: str = None, label: str = None, help_text: str = None):
+    def __init__(self, name: str = None, label: str = None, help_text: str = None, default=None):
         """
         :param str name: The attribute's machine name.
         :param str label: The attribute's label, a human readable name.
         :param str help_text: A short description to help understanding what the attribute is for.
+        :param default: A default value for this attribute.
         """
         self.name = name
         self.label = label
         self.help_text = help_text
+        self.default = default
